2a87fd32ff512d911099002f93276e54403ce0b9
Hawtian Wang <twistoy.wang@gmail.com>
2023-05-28 14:14:48 +0800

    fix nil check

--- lua/cpp-toolkit/functions/gen_def.lua
+++ lua/cpp-toolkit/functions/gen_def.lua
@@ -1,15 +1,17 @@
 local M = {}
 
-local Sig = require 'cpp-toolkit.treesitter.signature'
-local vt_preview = require 'cpp-toolkit.util.vt_preview'
+local Sig = require("cpp-toolkit.treesitter.signature")
+local vt_preview = require("cpp-toolkit.util.vt_preview")
 
 local cpptoolkit_copyed_function_declaration = {}
 function M.gen_function_declaration_at_cursor()
   cpptoolkit_copyed_function_declaration = Sig.function_declaration_at_cursor()
   if cpptoolkit_copyed_function_declaration ~= nil then
-    local preview = vt_preview.new_vt_preview(
-                        cpptoolkit_copyed_function_declaration:to_lines())
-    preview:mount()
+    local lines = cpptoolkit_copyed_function_declaration:to_lines()
+    if lines ~= nil then
+      local preview = vt_preview.new_vt_preview()
+      preview:mount()
+    end
   end
 end
 
